9d27a2cf72a3898e91dcbe5416044d31f29a1e3f
fuji0920 <69977636+fuji0808@users.noreply.github.com>
2020-10-04 21:15:09 +0900

    comit

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,11 @@ FactoryBot.define do
   factory :user do
     name {Faker::Name.last_name}
     email {Faker::Internet.free_email}
+<<<<<<< Updated upstream
     password = Faker::Internet.password(min_length: 6)
+=======
+    password = Faker::Internet.password(min_length: 8)
+>>>>>>> Stashed changes
     password {password}
     password_confirmation {password}
   end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -20,7 +20,11 @@ require 'rspec/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
+<<<<<<< Updated upstream
 Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+=======
+# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+>>>>>>> Stashed changes
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -31,10 +35,18 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 I18n.locale = "en"
+<<<<<<< Updated upstream
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.include SignInSupport
+=======
+
+RSpec.configure do |config|
+  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+>>>>>>> Stashed changes
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
